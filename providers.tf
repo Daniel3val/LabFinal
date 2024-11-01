fbbc11bc0bbede5ac531659542784ba09d30b6d0
Daniel3val <daniel1val@hotmail.es>
2024-11-02 00:19:18 +0100

    feat: added terraform files

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.account_id}:role/TerraformDeployRole"
+    session_name = "TerraformDeploySession"
+  }
+}
